3b0ac1b54a2464c5e880d009ba898181b781fefa
King Josh <robles.kingjosh0@gmail.com>
2026-06-01 10:37:16 -0700

    Initialize database for serverless API

--- server.js
+++ server.js
@@ -86,10 +86,20 @@ const subjects = [
 ];
 
 const subjectIds = new Set(subjects.map((subject) => subject.id));
+const dbReady = initDb();
 
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static(__dirname));
+
+app.use("/api", async (req, res, next) => {
+  try {
+    await dbReady;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
 
 function signToken(user) {
   return jwt.sign({ sub: user.id, email: user.email }, JWT_SECRET, { expiresIn: TOKEN_TTL });
@@ -253,7 +263,7 @@ app.use((error, req, res, next) => {
 });
 
 async function start() {
-  await initDb();
+  await dbReady;
   app.listen(PORT, () => {
     console.log(`SemStack running at http://localhost:${PORT}`);
   });
