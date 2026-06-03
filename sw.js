a99bc86efe444384a82b702a30903eac2232ae66
King Josh <robles.kingjosh0@gmail.com>
2026-06-03 09:45:35 -0700

    Return to login after registration

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "semstack-shell-v8";
+const CACHE_NAME = "semstack-shell-v9";
 const CORE_ASSETS = ["/", "/index.html", "/manifest.webmanifest", "/icons/semstack-icon.svg"];
 
 async function cacheResponse(request, response) {
